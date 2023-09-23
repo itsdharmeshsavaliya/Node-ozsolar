ec2a3d8a3cbcd745a9d1c591b54ab3a0e0ea66ff
Saddam Hussain <Saddam@maceit.co.nz>
2023-09-23 12:56:13 +0530

    aS

--- schema/tokenadmin.js
+++ schema/tokenadmin.js
@@ -1,20 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
 const TokenAdminSchema = new mongoose.Schema({
-    userId: {
-      type: Schema.Types.ObjectId,
-      required: true
-    },
-    token: { type: String, unique: true },
-    expiresAt: {
-        type: Date,
-        required: true,
-        // expires: 60*60*5 // token expires in 60 seconds
-      }
-
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  token: { type: String, unique: true },
+  expiresAt: {
+    type: Date,
+    required: true,
+    expires: 60 * 60 * 5, // token expires in 60 seconds
+  },
 });
 
-TokenAdminSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 3600*24 });
+TokenAdminSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 3600 * 24 });
 
-export default mongoose.model('TokenAdmin', TokenAdminSchema, 'AdminTokens');
-
+export default mongoose.model("TokenAdmin", TokenAdminSchema, "AdminTokens");
